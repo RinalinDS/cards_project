d144c3eee69756062c8976f243624becffb4b89d
RinalisDS <denispilyutin@gmail.com>
2022-05-31 00:11:28 +0300

    deleted unused request

--- client/src/api/cardsApi.ts
+++ client/src/api/cardsApi.ts
@@ -1,22 +1,12 @@
 import { AxiosResponse } from 'axios'
 
-import {CardT, CardTypePartial, UpdatedGradeRequestT, UpdatedGradeT} from '../types/PackTypes'
+import { CardT, CardTypePartial, UpdatedGradeRequestT, UpdatedGradeT } from '../types/PackTypes'
 
 import { instance } from './config'
 
-import {CardsPackT, GetPacksPayload, GetPacksResponseT} from 'types/PacksType'
+import { GetPacksPayload } from 'types/PacksType'
 
 export const cardsApi = {
-  setPack() {
-    const data = {
-      cardsPack: {
-        name: 'name',
-        deckCover: 'some cover12',
-        private: false,
-      },
-    }
-    const res = instance.post('cards/pack', data)
-  },
   getPacks: (payload: GetPacksPayload): Promise<AxiosResponse> => {
     // eslint-disable-next-line @typescript-eslint/no-magic-numbers
     const { packName, min, sortPacks, userId, max, pageCount = 10, page } = payload
