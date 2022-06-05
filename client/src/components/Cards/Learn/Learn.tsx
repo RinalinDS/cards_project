25dccfbe461e55b91ee7de0defd1d3428a4f2860
RinalisDS <denispilyutin@gmail.com>
2022-06-05 22:19:49 +0300

    half fix to learn process

--- client/src/components/Cards/Learn/Learn.tsx
+++ client/src/components/Cards/Learn/Learn.tsx
@@ -1,10 +1,10 @@
-import React, { useEffect, useState } from 'react'
+import React, {useEffect, useState} from 'react'
 
-import { EHelpers } from '../../../enums'
-import { SagaActions } from '../../../enums/sagaActions'
-import { useAppDispatch, useAppSelector } from '../../../hooks'
-import { CardT } from '../../../types/PackTypes'
-import { Button } from '../../common'
+import {EHelpers} from '../../../enums'
+import {SagaActions} from '../../../enums/sagaActions'
+import {useAppDispatch, useAppSelector} from '../../../hooks'
+import {CardT} from '../../../types/PackTypes'
+import {Button} from '../../common'
 
 import s from './Learn.module.css'
 
@@ -21,9 +21,9 @@ const getCard = (cards: CardT[]): CardT => {
   const res = cards.reduce(
     (acc: { sum: number; id: number }, card, i) => {
       const newSum = acc.sum + (EHelpers.Six - card.grade) * (EHelpers.Six - card.grade)
-      return { sum: newSum, id: newSum < rand ? i : acc.id }
+      return {sum: newSum, id: newSum < rand ? i : acc.id}
     },
-    { sum: 0, id: -1 },
+    {sum: 0, id: -1},
   )
   return cards[res.id + EHelpers.One]
 }
@@ -32,13 +32,13 @@ type PropsType = {
   deactivateModal: (value: string) => void
 }
 
-const LearnPage: React.FC<PropsType> = ({ deactivateModal }) => {
+const LearnPage: React.FC<PropsType> = ({deactivateModal}) => {
   const [isChecked, setIsChecked] = useState<boolean>(false)
   const [first, setFirst] = useState<boolean>(true)
   const currentPackID = useAppSelector(state => state.cards.currentPackId)
   const cards = useAppSelector(state => state.cards.currentPack?.cards) // FIX IT потому что я не знаю где брать пакИД ?
   const cardsTotalCount = useAppSelector(state => state.cards.currentPack?.cardsTotalCount)
-  const [rateCardInfo, setRateCardInfo] = useState<{cardId: string, grade:number}>({cardId: '', grade: 0})
+  const [rateCardInfo, setRateCardInfo] = useState<{ cardId: string, grade: number }>({cardId: '', grade: 0})
 
   const [card, setCard] = useState<CardT>({} as CardT)
 
@@ -48,17 +48,17 @@ const LearnPage: React.FC<PropsType> = ({ deactivateModal }) => {
     if (first) {
       dispatch({
         type: SagaActions.GetOnePack,
-        payload: { cardsPack_id: currentPackID, max: cardsTotalCount },
+        payload: {cardsPack_id: currentPackID, max: cardsTotalCount},
       })
+      if (cards?.length > EHelpers.Zero) setCard(getCard(cards))
       setFirst(false)
     }
-    if (cards?.length > EHelpers.Zero) setCard(getCard(cards))
 
     return () => {
     }
   }, [dispatch, cards, first])
 
-  const onNext = (): void => {
+  const onNext =  (): void => {
     setIsChecked(false)
     setRateCardInfo({cardId: '', grade: 0})
     dispatch(rateCard({card_id: rateCardInfo.cardId, grade: rateCardInfo.grade}))
@@ -66,6 +66,8 @@ const LearnPage: React.FC<PropsType> = ({ deactivateModal }) => {
       setCard(getCard(cards))
     }
   }
+
+  console.log({cards, card})
 
   return (
     /* можно тут если тру , то стили менять карточки, тогда вроде норм будет? */
@@ -83,9 +85,12 @@ const LearnPage: React.FC<PropsType> = ({ deactivateModal }) => {
             <span className={s.text}>Rate Yourself:</span>
 
             {grades.map((g, i) => (
+
+              <Button className={s.option} key={`grade-${i}`} onClick={() => {
                 // eslint-disable-next-line react/no-array-index-key,no-underscore-dangle
-                <Button className={s.option} key={`grade-${i}`} onClick={() => {setRateCardInfo({cardId: card._id, grade: i+EHelpers.One})}}>
-                  <span className={s.optionText}>{g}</span>
+                setRateCardInfo({cardId: card._id, grade: i + EHelpers.One})
+              }}>
+                <span className={s.optionText}>{g}</span>
               </Button>
             ))}
           </div>
